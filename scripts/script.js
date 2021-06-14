2871ece11214be9b37d0e542aff4f2a957b40179
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-13 22:45:06 -0300

    criado funcao para inserir cartas(divs) no tabuleiro(board-html)

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,7 @@
-const front = 'card_front';
-const back = 'card_back';
+const FRONT = 'card-front';
+const BACK = 'card-back';
+const CARD = 'card';
+const ICON = 'icon';
 let cards = null;
 
 let techs = [
@@ -20,7 +22,43 @@ startGame();
 function startGame(){
     cards = createCardsFromTechs(techs);
     shuffleCards(cards);
-    console.log(cards)
+    initializeCards(cards);
+}
+
+function initializeCards(cards){
+    let gameBoard = document.getElementById('gameBoard');
+
+    cards.forEach(card => {
+        let cardElement = document.createElement('div');
+        cardElement.id = card.id;
+        cardElement.classList.add(CARD);
+        cardElement.dataset.icon = card.icon;
+
+        createCardContent(card, cardElement);
+
+        cardElement.addEventListener('click', flipCard);
+        gameBoard.appendChild(cardElement);
+
+    });
+}
+
+function createCardContent(card, cardElement){
+    createCardFace(FRONT, card, cardElement);
+    createCardFace(BACK, card, cardElement);
+}
+
+function createCardFace(face, card, element){
+    let cardElementFace = document.createElement('div');
+    cardElementFace.classList.add(face);
+    if(face === FRONT){
+        let iconElement = document.createElement('img');
+        iconElement.classList.add(ICON);
+        iconElement.src = './assets/' + card.icon + '.png';
+        cardElementFace.appendChild(iconElement);
+    }else{
+        cardElementFace.innerHTML = "&lt;/&gt;";
+    }
+    element.appendChild(cardElementFace);
 }
 
 function shuffleCards(cards){
@@ -40,9 +78,9 @@ function createCardsFromTechs(techs){
 
     let cards = [];
 
-    for(let tech of techs){
+    techs.forEach((tech) => {
         cards.push(createPairFromTech(tech));
-    }
+    })
 
     return cards.flatMap(pair => pair);
 }
@@ -62,4 +100,8 @@ function createPairFromTech(tech){
 
 function createIdWithTech(tech){
     return tech + parseInt(Math.random() * 1000);
+}
+
+function flipCard(){
+    this.classList.add('flip')
 }
