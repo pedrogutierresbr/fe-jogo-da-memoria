81d42d8b754cd61be0e253bfccdd7de561eebd1e
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-14 00:23:40 -0300

    adicionado funcao checar game over e restart

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ function startGame(){
 
 function initializeCards(cards){
     let gameBoard = document.getElementById('gameBoard');
-
+    gameBoard.innerHTML = '';
     game.cards.forEach(card => {
         let cardElement = document.createElement('div');
         cardElement.id = card.id;
@@ -53,6 +53,10 @@ function flipCard(){
         if(game.secondCard){
             if(game.checkMatch()){
                 game.clearCards();
+                if(game.checkGameOver()){
+                    let gameOverLayer = document.getElementById('gameOver');
+                    gameOverLayer.style.display = 'flex';
+                }
             }else{
                 setTimeout(() => {
                     let firstCardView = document.getElementById(game.firstCard.id);
@@ -67,3 +71,10 @@ function flipCard(){
         }
     }
 }
+
+function restart(){
+    game.clearCards();
+    startGame();
+    let gameOverLayer = document.getElementById('gameOver');
+    gameOverLayer.style.display = 'none';
+};
