51449fb13bf74fcddc66b34d892f9ca1fd8888d4
Pedro Gutieres <pedro-gabriel1@hotmail.com>
2021-06-13 21:14:54 -0300

    criado modelo de identificacao de cada carta no jogo

---
+++ scripts/script.js
@@ -0,0 +1,45 @@
+const front = 'card_front';
+const back = 'card_back';
+
+let techs = [
+    'bootstrap',
+    'css',
+    'electron',
+    'firebase',
+    'html',
+    'javascript',
+    'jquery',
+    'mongo',
+    'node',
+    'react'
+];
+
+createCardsFromTechs(techs);
+
+function createCardsFromTechs(techs){
+
+    let cards = [];
+
+    for(let tech of techs){
+        cards.push(createPairFromTech(tech));
+    }
+
+    return cards.flatMap(pair => pair);
+}
+
+function createPairFromTech(tech){
+
+    return [{
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false
+    },{
+        id: createIdWithTech(tech),
+        icon: tech,
+        flipped: false
+    }]
+}
+
+function createIdWithTech(tech){
+    return tech + parseInt(Math.random() * 1000);
+}
